8b720c0ee7f742c47bd0511a8942ef6ca9ad0662
Eaglet12IQ <eaglet12iq@gmail.com>
2026-03-27 11:22:29 +0300

    feat: add version property to ModuleA and ModuleB

--- node/modules/cycleA.js
+++ node/modules/cycleA.js
@@ -1,6 +1,7 @@
 // Модуль A - начало цикла
 export default {
   name: "ModuleA",
+  version: "1.0.0",
   requires: ["ModuleB"],
   register(container) {
     container.addSingleton("action.moduleA", () => ({
--- node/modules/cycleB.js
+++ node/modules/cycleB.js
@@ -1,6 +1,7 @@
 // Модуль B - конец цикла (зависит от A)
 export default {
   name: "ModuleB",
+  version: "1.0.0",
   requires: ["ModuleA"],
   register(container) {
     container.addSingleton("action.moduleB", () => ({
